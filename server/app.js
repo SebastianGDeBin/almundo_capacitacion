113d942c758a2a030972ea8ba77a9220f5584b58
Sebastian De Bin <sebastiandebin@live.com.ar>
2017-03-10 11:46:26 -0300

    ejercicio con post get

--- server/app.js
+++ server/app.js
@@ -3,11 +3,27 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.set('views', path.join(__dirname + '/../public'));
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  res.render('hoteles', {title:'Resultado de hoteles', nameHotel:'Nombre del Hotel', titleFilter:'Regimenes'});
+	res.render('hoteles', {title:'Resultado de hoteles', nameHotel:'Nombre del Hotel', titleFilter:'Regimenes'});
+});
+
+app.get('/users', function(req,res){
+	res.render('users',{nombre:'nombre',edad:'edad'});
+});
+
+app.post('/users/create', function (req, res) {
+	console.log(req.body);
+	res.json(req.body)
 });
 
 app.listen(3000);
