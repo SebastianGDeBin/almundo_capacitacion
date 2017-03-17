bcedeb7b8dd18886830a677affe8b883f2150bec
Sebastian De Bin <sebastiandebin@live.com.ar>
2017-03-17 09:51:25 -0300

    acomodando el repo

--- public/js/app/app.template.js
+++ public/js/app/app.template.js
@@ -1,6 +1,1 @@
-
-(function() {
-  angular.module('App', ['components']);
-})();
-
 (function() { angular.module("App").run(["$templateCache", function($templateCache) {$templateCache.put('components/first-comp/first-comp.html','<div><h1>Hola</h1><p class="estilo"> {{$ctrl.name}}</p></div>');}])})();
--- public/js/components/components-pkg.js
+++ public/js/components/components-pkg.js
@@ -7,37 +7,13 @@
 })();
 
 (function() {
-  angular.module('components', ['firstComp']);
-})();
-
-(function() {
-  angular.module('firstComp', []);
-})();
-
-(function() {
   angular
     .module('firstComp')
     .component('firstComp', {
-      templateUrl: 'first-comp.html',
+      templateUrl: 'components/first-comp/first-comp.html',
       controller: firstCompController,
       bindings: {
-        hola: '@'
-      }
-  });
-
-  function firstCompController() {
-    var _self = this;
-  }
-})();
-
-(function() {
-  angular
-    .module('firstComp')
-    .component('firstComp', {
-      templateUrl: 'first-comp.html',
-      controller: firstCompController,
-      bindings: {
-        hola: '@'
+        name: '@'
       }
   });
 
